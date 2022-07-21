409ff741b3b4b6209a9eaf91651bcd2aba2dfdcd
Cacsdeveloper <109742526+Cacsdeveloper@users.noreply.github.com>
2022-07-21 09:37:03 -0400

    primera subida

---
+++ index.js
@@ -0,0 +1,11 @@
+/*console.log("Hola estoy oculto fuera del doom");
+const div = document.querySelector("body");
+div.innerHTML = 'se logro';*/
+//let $Name = Diego;
+document.write("hola ");
+document.write("Texto con función write en js");
+/*
+alert("mensaje usando la función alert");
+alert("Sumatoria con alert: " + (1+2+3+4+5));
+alert("Sumatoria de caracteres con: " + '"Alert"');
+["Bienvenidos", "CodingTube"].forEach(alert);*/
